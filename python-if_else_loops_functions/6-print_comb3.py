4be1b80d331de79e82e24289fe5c498562db4c1f
Umukundwaa <a.umukundwa1@alustudent.com>
2025-07-01 07:04:10 -0700

    print 6 comb3 file

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
-    for j in range(10):
-        if i < j:
-            if i == 8 and j == 9:
-                print(f"{i}{j}")
-            else:
-                print(f"{i}{j}", end=", ")
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print(f"{i}{j}")
+        else:
+            print(f"{i}{j}", end=", ")
