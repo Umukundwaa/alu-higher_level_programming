6ed162da43e586d276bbd8ab5a477c570a912912
Umukundwaa <a.umukundwa1@alustudent.com>
2025-07-09 15:17:56 +0200

    update

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,12 +1,5 @@
+#!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    # Make a copy of the original list
-    new_list = my_list[:]
-
-    # Check if the index is valid
-    if idx < 0 or idx >= len(my_list):
-        return new_list
-
-    # Replace the element at the specified index
-    new_list[idx] = element
-
-    return new_list
+    if not (0 <= idx < len(my_list)):
+        return my_list.copy()
+    return my_list[:idx] + [element] + my_list[idx + 1:]
