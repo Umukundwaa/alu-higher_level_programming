f4a445e7747fc9344e7bdfeae4d4a9bad9bd9abd
Umukundwaa <a.umukundwa1@alustudent.com>
2025-07-10 14:50:37 +0200

    02-main.py file

---
+++ python-exceptions/02-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+magic_calculation = __import__('102-magic_calculation').magic_calculation
+
+# Test cases
+print("magic_calculation(1, 2) =", magic_calculation(1, 2))
+print("magic_calculation(3, 2) =", magic_calculation(3, 2))
+print("magic_calculation(0, 5) =", magic_calculation(0, 5))
+print("magic_calculation(2, 3) =", magic_calculation(2, 3))
