b56e61f05d359f4b8aaeaf30cb3a7cfbc0af112a
Umukundwaa <a.umukundwa1@alustudent.com>
2025-07-11 20:05:27 +0200

    100-singly_linked_list.py

---
+++ python-classes/100-singly_linked_list.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""Defines classes for a singly linked list and its nodes."""
+
+
+class Node:
+    """Node of a singly linked list."""
+
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """Retrieve the data."""
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        """Set the data with validation."""
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        """Retrieve the next node."""
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """Set the next node with validation."""
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """Defines a singly linked list."""
+
+    def __init__(self):
+        self.__head = None
+
+    def sorted_insert(self, value):
+        """Insert a new Node into the correct sorted position (increasing order)."""
+        new_node = Node(value)
+
+        # Case 1: List is empty or value should be new head
+        if self.__head is None or value < self.__head.data:
+            new_node.next_node = self.__head
+            self.__head = new_node
+            return
+
+        # Case 2: Traverse and insert at proper place
+        current = self.__head
+        while current.next_node is not None and current.next_node.data < value:
+            current = current.next_node
+
+        new_node.next_node = current.next_node
+        current.next_node = new_node
+
+    def __str__(self):
+        """Return string representation of the entire list, one data per line."""
+        values = []
+        current = self.__head
+        while current is not None:
+            values.append(str(current.data))
+            current = current.next_node
+        return "\n".join(values)
