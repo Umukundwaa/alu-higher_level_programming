b499d0dac77fd68c2177eb2acbd5c331719a82a5
Umukundwaa <a.umukundwa1@alustudent.com>
2025-07-11 20:18:05 +0200

    103-magic_class.py file

---
+++ python-classes/103-magic_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines MagicClass matching given bytecode behavior."""
+
+import math
+
+
+class MagicClass:
+    """Class with a private radius attribute, area, and circumference methods."""
+
+    def __init__(self, radius=0):
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """Return the area of the circle."""
+        return (self.__radius ** 2) * math.pi
+
+    def circumference(self):
+        """Return the circumference of the circle."""
+        return 2 * math.pi * self.__radius
