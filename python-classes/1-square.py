b2a017984da8d1e524f36011cc0b10af28e74795
Umukundwaa <a.umukundwa1@alustudent.com>
2025-07-11 19:30:54 +0200

    1-square.py file

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a class Square with private size attribute"""
+
+
+class Square:
+    """Square class with private instance attribute size"""
+
+    def __init__(self, size):
+        self.__size = size
